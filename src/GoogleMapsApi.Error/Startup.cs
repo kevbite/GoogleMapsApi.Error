33da42636ea6fdd2127238e7c98a931e9ecf0823
kevbite <kev_bite@msn.com>
2015-11-07 22:04:58 +0000

    Adding example of error with GoogleMapsApi package.

---
+++ src/GoogleMapsApi.Error/Startup.cs
@@ -0,0 +1,17 @@
+﻿using GoogleMapsApi.Error;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace GoogleMapsApi.Error
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
---
+++ src/GoogleMapsApi.Error/TestHub.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using GoogleMapsApi.Entities.PlacesText.Request;
+using GoogleMapsApi.Entities.PlacesText.Response;
+using Microsoft.AspNet.SignalR;
+
+namespace GoogleMapsApi.Error
+{
+    public class TestHub : Hub
+    {
+        private readonly PlacesTextRequest _request;
+
+        public TestHub()
+        {
+            _request = new PlacesTextRequest()
+            {
+                ApiKey = "{need-api-key}",
+                Query = "Cineworld"
+            };
+            
+        }
+
+        public async Task<PlacesTextResponse> SearchGooglePlacesAsync()
+        {
+            return await GoogleMaps.PlacesText.QueryAsync(_request).ConfigureAwait(false);
+        }
+
+        public PlacesTextResponse SearchGooglePlacesSync()
+        {
+            return GoogleMaps.PlacesText.Query(_request);
+        }
+    }
+}
